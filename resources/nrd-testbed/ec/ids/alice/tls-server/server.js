430ee8db4cf17e407bae2d7df15df1d11c4d175e
Simon Petrac <pfoerdie@gmail.com>
2021-10-04 16:46:56 +0200

    removed testbed certs due to an error

--- resources/nrd-testbed/ec/ids/alice/tls-server/server.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs'), path = require('path'), crypto = require('crypto');
-exports.meta = require('./server.json');
-exports.key = fs.readFileSync(path.join(__dirname, './server.key'));
-exports.privateKey = crypto.createPrivateKey(exports.key);
-exports.pub = fs.readFileSync(path.join(__dirname, './server.pub'));
-exports.publicKey = crypto.createPublicKey(exports.pub);
-exports.cert = fs.readFileSync(path.join(__dirname, './server.cert'));
-Object.freeze(exports);
