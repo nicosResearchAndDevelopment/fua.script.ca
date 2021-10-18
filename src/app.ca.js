bbd2c1d8a4f1ed1905f5455647da3b2d5e5754c2
jlangkau <jlangkau@nicos-rd.com>
2021-10-18 17:15:26 +0200

    work

--- src/app.ca.js
+++ src/app.ca.js
@@ -74,35 +74,41 @@ module.exports = async function ({agent, config}) {
             });
         } // if (shield)
 
+        // REM : io :: ids to Alice, Bob
+        //await agent.generateClientCertificate('nrd-testbed/ec/ids/component/io/client', {
+        //    ca:      'nrd-testbed/ec/ids/component/ca',
+        //    subject: {C, ST, L, O, CN: ['tb', 'ec', 'ids', 'io']}
+        //});
+
         // REM : nrd-daps
-        await agent.generateClientCertificate('nrd-testbed/ec/ids/component/daps_nrd/connector/client', {
-            ca:      'nrd-testbed/ec/ids/component/ca',
-            subject: {C, ST, L, O, CN: ['tb', 'ec', 'ids', 'ec_ids_daps_nrd']} // REM : 'ec_ids_daps_nrd' = DAPS 'nrd' (nicos Research & Development GmbH)
-        });
-        await agent.generateClientCertificate('nrd-testbed/ec/ids/component/daps_nrd/tls-server/server', {
-            ca:      'nrd-testbed/ec/ids/component/ca',
-            subject: {C, ST, L, O, CN: ['tb', 'ec', 'ids', 'ec_ids_daps_nrd_tls-server']}
-        });
+        //await agent.generateClientCertificate('nrd-testbed/ec/ids/component/daps_nrd/connector/client', {
+        //    ca:      'nrd-testbed/ec/ids/component/ca',
+        //    subject: {C, ST, L, O, CN: ['tb', 'ec', 'ids', 'ec_ids_daps_nrd']} // REM : 'ec_ids_daps_nrd' = DAPS 'nrd' (nicos Research & Development GmbH)
+        //});
+        //await agent.generateClientCertificate('nrd-testbed/ec/ids/component/daps_nrd/tls-server/server', {
+        //    ca:      'nrd-testbed/ec/ids/component/ca',
+        //    subject: {C, ST, L, O, CN: ['tb', 'ec', 'ids', 'ec_ids_daps_nrd_tls-server']}
+        //});
 
         // REM : omejdn-daps
-        await agent.generateClientCertificate('nrd-testbed/ec/ids/component/daps_fh_omejdn/connector/client', {
-            ca:      'nrd-testbed/ec/ids/component/ca',
-            subject: {C, ST, L, O, CN: ['tb', 'ec', 'ids', 'ec_ids_daps_fh_omejdn']} // REM : 'ec_ids_daps_fh_omejdn' = DAPS 'omejdn' (Fraunhofer, AISEC)
-        });
-        await agent.generateClientCertificate('nrd-testbed/ec/ids/component/daps_fh_omejdn/tls-server/server', {
-            ca:      'nrd-testbed/ec/ids/component/ca',
-            subject: {C, ST, L, O, CN: ['tb', 'ec', 'ids', 'ec_ids_daps_fh_omejdn_tls-server']}
-        });
+        //await agent.generateClientCertificate('nrd-testbed/ec/ids/component/daps_fh_omejdn/connector/client', {
+        //    ca:      'nrd-testbed/ec/ids/component/ca',
+        //    subject: {C, ST, L, O, CN: ['tb', 'ec', 'ids', 'ec_ids_daps_fh_omejdn']} // REM : 'ec_ids_daps_fh_omejdn' = DAPS 'omejdn' (Fraunhofer, AISEC)
+        //});
+        //await agent.generateClientCertificate('nrd-testbed/ec/ids/component/daps_fh_omejdn/tls-server/server', {
+        //    ca:      'nrd-testbed/ec/ids/component/ca',
+        //    subject: {C, ST, L, O, CN: ['tb', 'ec', 'ids', 'ec_ids_daps_fh_omejdn_tls-server']}
+        //});
 
         // REM : Meta Data Broker
-        await agent.generateClientCertificate('nrd-testbed/ec/ids/component/mdb_fh_mdb/connector/client', {
-            ca:      'nrd-testbed/ec/ids/component/ca',
-            subject: {C, ST, L, O, CN: ['tb', 'ec', 'ids', 'ec_ids_mdb_fh_mdb']} // REM : 'ec_ids_mdb_fh_mdb' = Meta Data Broker Fraunhofer
-        });
-        await agent.generateClientCertificate('nrd-testbed/ec/ids/component/mdb_fh_mdb/tls-server/server', {
-            ca:      'nrd-testbed/ec/ids/component/ca',
-            subject: {C, ST, L, O, CN: ['tb', 'ec', 'ids', 'ec_ids_mdb_fh_mdb_tls-server']}
-        });
+        //await agent.generateClientCertificate('nrd-testbed/ec/ids/component/mdb_fh_mdb/connector/client', {
+        //    ca:      'nrd-testbed/ec/ids/component/ca',
+        //    subject: {C, ST, L, O, CN: ['tb', 'ec', 'ids', 'ec_ids_mdb_fh_mdb']} // REM : 'ec_ids_mdb_fh_mdb' = Meta Data Broker Fraunhofer
+        //});
+        //await agent.generateClientCertificate('nrd-testbed/ec/ids/component/mdb_fh_mdb/tls-server/server', {
+        //    ca:      'nrd-testbed/ec/ids/component/ca',
+        //    subject: {C, ST, L, O, CN: ['tb', 'ec', 'ids', 'ec_ids_mdb_fh_mdb_tls-server']}
+        //});
 
         // REM : ParIS
         // await agent.generateClientCertificate('nrd-testbed/ec/ids/paris/connector/client', {
@@ -135,24 +141,24 @@ module.exports = async function ({agent, config}) {
         // });
 
         // REM : ALICE
-        await agent.generateClientCertificate('nrd-testbed/ec/ids/component/alice/connector/client', {
-            ca:      'nrd-testbed/ec/ids/component/ca',
-            subject: {C, ST, L, O, CN: ['tb', 'ec', 'ids', 'ec_ids_connector_alice']}
-        });
-        await agent.generateClientCertificate('nrd-testbed/ec/ids/component/alice/tls-server/server', {
-            ca:      'nrd-testbed/ec/ids/component/ca',
-            subject: {C, ST, L, O, CN: ['tb', 'ec', 'ids', 'ec_ids_connector_alice_tls-server']}
-        });
+        //await agent.generateClientCertificate('nrd-testbed/ec/ids/component/alice/connector/client', {
+        //    ca:      'nrd-testbed/ec/ids/component/ca',
+        //    subject: {C, ST, L, O, CN: ['tb', 'ec', 'ids', 'ec_ids_connector_alice']}
+        //});
+        //await agent.generateClientCertificate('nrd-testbed/ec/ids/component/alice/tls-server/server', {
+        //    ca:      'nrd-testbed/ec/ids/component/ca',
+        //    subject: {C, ST, L, O, CN: ['tb', 'ec', 'ids', 'ec_ids_connector_alice_tls-server']}
+        //});
 
         // REM : BOB
-        await agent.generateClientCertificate('nrd-testbed/ec/ids/component/bob/connector/client', {
-            ca:      'nrd-testbed/ec/ids/component/ca',
-            subject: {C, ST, L, O, CN: ['tb', 'ec', 'ids', 'ec_ids_connector_bob']}
-        });
-        await agent.generateClientCertificate('nrd-testbed/ec/ids/component/bob/tls-server/server', {
-            ca:      'nrd-testbed/ec/ids/component/ca',
-            subject: {C, ST, L, O, CN: ['tb', 'ec', 'ids', 'ec_ids_connector_bob_tls-server']}
-        });
+        //await agent.generateClientCertificate('nrd-testbed/ec/ids/component/bob/connector/client', {
+        //    ca:      'nrd-testbed/ec/ids/component/ca',
+        //    subject: {C, ST, L, O, CN: ['tb', 'ec', 'ids', 'ec_ids_connector_bob']}
+        //});
+        //await agent.generateClientCertificate('nrd-testbed/ec/ids/component/bob/tls-server/server', {
+        //    ca:      'nrd-testbed/ec/ids/component/ca',
+        //    subject: {C, ST, L, O, CN: ['tb', 'ec', 'ids', 'ec_ids_connector_bob_tls-server']}
+        //});
 
         // REM : Data Space Connector
         // await agent.generateClientCertificate('nrd-testbed/ec/ids/dsc/connector/client', {
@@ -165,24 +171,27 @@ module.exports = async function ({agent, config}) {
         // });
 
         //region TEST tb.ec.ids : cut (applicant)
+
         // REM : GAIAboX
-        await agent.generateClientCertificate('nrd-testbed/ec/ids/cut/nrd_gbx-03/connector/client', {
-            ca:      'nrd-testbed/ec/ids/component/ca',
-            subject: {C, ST, L, O, CN: ['tb', 'ec', 'ids', 'cut', 'ec_ids_connector_nrd_gbx-03']}
-        });
-        await agent.generateClientCertificate('nrd-testbed/ec/ids/cut/nrd_gbx-03/tls-server/server', {
-            ca:      'nrd-testbed/ec/ids/component/ca',
-            subject: {C, ST, O, CN: ['tb', 'ec_ids_connector_nrd_gbx-03_tls-server']}
-        });
+        //await agent.generateClientCertificate('nrd-testbed/ec/ids/cut/nrd_gbx-03/connector/client', {
+        //    ca:      'nrd-testbed/ec/ids/component/ca',
+        //    subject: {C, ST, L, O, CN: ['tb', 'ec', 'ids', 'cut', 'ec_ids_connector_nrd_gbx-03']}
+        //});
+        //await agent.generateClientCertificate('nrd-testbed/ec/ids/cut/nrd_gbx-03/tls-server/server', {
+        //    ca:      'nrd-testbed/ec/ids/component/ca',
+        //    subject: {C, ST, O, CN: ['tb', 'ec_ids_connector_nrd_gbx-03_tls-server']}
+        //});
+
         // REM : Data Space Connector (dsc)
-        await agent.generateClientCertificate('nrd-testbed/ec/ids/cut/fh_dsc/connector/client', {
-            ca:      'nrd-testbed/ec/ids/component/ca',
-            subject: {C, ST, L, O, CN: ['tb', 'ec', 'ids', 'cut', 'ec_ids_connector_fh_dsc']}
-        });
-        await agent.generateClientCertificate('nrd-testbed/ec/ids/cut/fh_dsc/tls-server/server', {
-            ca:      'nrd-testbed/ec/ids/component/ca',
-            subject: {C, ST, O, CN: ['tb', 'ec_ids_fh_connector_dsc_tls-server']}
-        });
+        //await agent.generateClientCertificate('nrd-testbed/ec/ids/cut/fh_dsc/connector/client', {
+        //    ca:      'nrd-testbed/ec/ids/component/ca',
+        //    subject: {C, ST, L, O, CN: ['tb', 'ec', 'ids', 'cut', 'ec_ids_connector_fh_dsc']}
+        //});
+        //await agent.generateClientCertificate('nrd-testbed/ec/ids/cut/fh_dsc/tls-server/server', {
+        //    ca:      'nrd-testbed/ec/ids/component/ca',
+        //    subject: {C, ST, O, CN: ['tb', 'ec_ids_fh_connector_dsc_tls-server']}
+        //});
+
         //endregion TEST tb.ec.ids : cut (applicant)
 
         //endregion TEST tb.ec.ids
