3658c8d94bf9117e86f3639d0c82619e8517dcec
Simon Petrac <pfoerdie@gmail.com>
2023-03-16 16:50:56 +0100

    created fiware certificates

--- src/code/generators/generator.nrd-testbed.js
+++ src/code/generators/generator.nrd-testbed.js
@@ -345,6 +345,7 @@ generators['nrd-testbed/ec/ids/cut']
     = async function (param) {
     await generators['nrd-testbed/ec/ids/cut/nrd_gbx-03'](param);
     await generators['nrd-testbed/ec/ids/cut/dsc'](param);
+    await generators['nrd-testbed/ec/ids/cut/FIWARE'](param);
 };
 
 generators['nrd-testbed/ec/ids/cut/nrd_gbx-03']
@@ -399,6 +400,109 @@ generators['nrd-testbed/ec/ids/cut/dsc/tls-server/server']
         subject: {
             ...defaults.subject,
             CN: ['tb', 'ids-dsc.nicos-rd.com']
+        }
+    });
+};
+
+generators['nrd-testbed/ec/ids/cut/FIWARE']
+    = async function (param) {
+    await generators['nrd-testbed/ec/ids/cut/FIWARE/dev'](param);
+    await generators['nrd-testbed/ec/ids/cut/FIWARE/car-kim'](param);
+    await generators['nrd-testbed/ec/ids/cut/FIWARE/platform-kim'](param);
+};
+
+generators['nrd-testbed/ec/ids/cut/FIWARE/dev']
+    = async function (param) {
+    await generators['nrd-testbed/ec/ids/cut/FIWARE/dev/connector/client'](param);
+    await generators['nrd-testbed/ec/ids/cut/FIWARE/dev/tls-server/server'](param);
+};
+
+generators['nrd-testbed/ec/ids/cut/FIWARE/dev/connector/client']
+    = async function ({agent, config, defaults}) {
+    await agent.generateClientCertificate('nrd-testbed/ec/ids/cut/FIWARE/dev/connector/client', {
+        ca:      'nrd-testbed/ec/ids/component/ca',
+        subject: {
+            country:             'DE',
+            organization:        'FIWARE Foundation e.V.',
+            organizational_unit: 'KIM',
+            common_name:         '*.fiware.dev'
+        }
+    });
+};
+
+generators['nrd-testbed/ec/ids/cut/FIWARE/dev/tls-server/server']
+    = async function ({agent, config, defaults}) {
+    await agent.generateClientCertificate('nrd-testbed/ec/ids/cut/FIWARE/dev/tls-server/server', {
+        ca:      'nrd-testbed/ec/ids/component/ca',
+        subject: {
+            country:             'DE',
+            organization:        'FIWARE Foundation e.V.',
+            organizational_unit: 'KIM',
+            common_name:         '*.fiware.dev'
+        }
+    });
+};
+
+generators['nrd-testbed/ec/ids/cut/FIWARE/car-kim']
+    = async function (param) {
+    await generators['nrd-testbed/ec/ids/cut/FIWARE/car-kim/connector/client'](param);
+    await generators['nrd-testbed/ec/ids/cut/FIWARE/car-kim/tls-server/server'](param);
+};
+
+generators['nrd-testbed/ec/ids/cut/FIWARE/car-kim/connector/client']
+    = async function ({agent, config, defaults}) {
+    await agent.generateClientCertificate('nrd-testbed/ec/ids/cut/FIWARE/car-kim/connector/client', {
+        ca:      'nrd-testbed/ec/ids/component/ca',
+        subject: {
+            country:             'DE',
+            organization:        'FIWARE Foundation e.V.',
+            organizational_unit: 'KIM',
+            common_name:         '*.car-kim.fiware-dataspace-connector.org'
+        }
+    });
+};
+
+generators['nrd-testbed/ec/ids/cut/FIWARE/car-kim/tls-server/server']
+    = async function ({agent, config, defaults}) {
+    await agent.generateClientCertificate('nrd-testbed/ec/ids/cut/FIWARE/car-kim/tls-server/server', {
+        ca:      'nrd-testbed/ec/ids/component/ca',
+        subject: {
+            country:             'DE',
+            organization:        'FIWARE Foundation e.V.',
+            organizational_unit: 'KIM',
+            common_name:         '*.car-kim.fiware-dataspace-connector.org'
+        }
+    });
+};
+
+generators['nrd-testbed/ec/ids/cut/FIWARE/platform-kim']
+    = async function (param) {
+    await generators['nrd-testbed/ec/ids/cut/FIWARE/platform-kim/connector/client'](param);
+    await generators['nrd-testbed/ec/ids/cut/FIWARE/platform-kim/tls-server/server'](param);
+};
+
+generators['nrd-testbed/ec/ids/cut/FIWARE/platform-kim/connector/client']
+    = async function ({agent, config, defaults}) {
+    await agent.generateClientCertificate('nrd-testbed/ec/ids/cut/FIWARE/platform-kim/connector/client', {
+        ca:      'nrd-testbed/ec/ids/component/ca',
+        subject: {
+            country:             'DE',
+            organization:        'FIWARE Foundation e.V.',
+            organizational_unit: 'KIM',
+            common_name:         '*.platform-kim.fiware-dataspace-connector.org'
+        }
+    });
+};
+
+generators['nrd-testbed/ec/ids/cut/FIWARE/platform-kim/tls-server/server']
+    = async function ({agent, config, defaults}) {
+    await agent.generateClientCertificate('nrd-testbed/ec/ids/cut/FIWARE/platform-kim/tls-server/server', {
+        ca:      'nrd-testbed/ec/ids/component/ca',
+        subject: {
+            country:             'DE',
+            organization:        'FIWARE Foundation e.V.',
+            organizational_unit: 'KIM',
+            common_name:         '*.platform-kim.fiware-dataspace-connector.org'
         }
     });
 };
