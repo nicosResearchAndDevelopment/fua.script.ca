fb68196e0462c9cd71ceddeb1973325ba7a920d9
Simon Petrac <pfoerdie@gmail.com>
2024-01-04 11:37:58 +0100

    added nova generator

--- src/code/generators/generator.nrd-testbed.cut.dbx.js
+++ src/code/generators/generator.nrd-testbed.cut.dbx.js
@@ -4,12 +4,14 @@ const
 
 Object.assign(generators, require('./generator.nrd-testbed.cut.dbx.swc.js'));
 Object.assign(generators, require('./generator.nrd-testbed.cut.dbx.sie.js'));
+Object.assign(generators, require('./generator.nrd-testbed.cut.dbx.nova.js'));
 Object.assign(generators, require('./generator.nrd-testbed.cut.dbx.dum.js'));
 
 generators['nrd-testbed/ec/ids/cut/DBX']
     = async function (param) {
     await generators['nrd-testbed/ec/ids/cut/DBX/SWC'](param);
     await generators['nrd-testbed/ec/ids/cut/DBX/SIE'](param);
+    await generators['nrd-testbed/ec/ids/cut/DBX/NOVA'](param);
     await generators['nrd-testbed/ec/ids/cut/DBX/DUM'](param);
 };
 
---
+++ src/code/generators/generator.nrd-testbed.cut.dbx.nova.js
@@ -0,0 +1,196 @@
+const
+    generators = exports,
+    util       = require('../util.ca.js');
+
+generators['nrd-testbed/ec/ids/cut/DBX/NOVA']
+    = async function (param) {
+    await generators['nrd-testbed/ec/ids/cut/DBX/NOVA/1'](param);
+    await generators['nrd-testbed/ec/ids/cut/DBX/NOVA/2'](param);
+    await generators['nrd-testbed/ec/ids/cut/DBX/NOVA/3'](param);
+    await generators['nrd-testbed/ec/ids/cut/DBX/NOVA/4'](param);
+    await generators['nrd-testbed/ec/ids/cut/DBX/NOVA/5'](param);
+    await generators['nrd-testbed/ec/ids/cut/DBX/NOVA/6'](param);
+};
+
+generators['nrd-testbed/ec/ids/cut/DBX/NOVA/1']
+    = async function (param) {
+    await generators['nrd-testbed/ec/ids/cut/DBX/NOVA/1/connector/client'](param);
+    await generators['nrd-testbed/ec/ids/cut/DBX/NOVA/1/tls-server/server'](param);
+};
+
+generators['nrd-testbed/ec/ids/cut/DBX/NOVA/1/connector/client']
+    = async function ({agent, config, defaults}) {
+    await agent.generateCustomerCertificate('nrd-testbed/ec/ids/cut/DBX/NOVA/1/connector/client', {
+        ca:      'nrd-testbed/ec/ids/component/ca',
+        subject: {
+            country:      'GR',
+            organization: 'Nova',
+            commonName:   '*.nova.gr'
+        }
+    });
+};
+
+generators['nrd-testbed/ec/ids/cut/DBX/NOVA/1/tls-server/server']
+    = async function ({agent, config, defaults}) {
+    await agent.generateCustomerCertificate('nrd-testbed/ec/ids/cut/DBX/NOVA/1/tls-server/server', {
+        ca:      'nrd-testbed/ec/ids/component/ca',
+        subject: {
+            country:      'GR',
+            organization: 'Nova',
+            commonName:   '*.nova.gr'
+        }
+    });
+};
+
+generators['nrd-testbed/ec/ids/cut/DBX/NOVA/2']
+    = async function (param) {
+    await generators['nrd-testbed/ec/ids/cut/DBX/NOVA/2/connector/client'](param);
+    await generators['nrd-testbed/ec/ids/cut/DBX/NOVA/2/tls-server/server'](param);
+};
+
+generators['nrd-testbed/ec/ids/cut/DBX/NOVA/2/connector/client']
+    = async function ({agent, config, defaults}) {
+    await agent.generateCustomerCertificate('nrd-testbed/ec/ids/cut/DBX/NOVA/2/connector/client', {
+        ca:      'nrd-testbed/ec/ids/component/ca',
+        subject: {
+            country:      'GR',
+            organization: 'Nova',
+            commonName:   '*.nova.gr'
+        }
+    });
+};
+
+generators['nrd-testbed/ec/ids/cut/DBX/NOVA/2/tls-server/server']
+    = async function ({agent, config, defaults}) {
+    await agent.generateCustomerCertificate('nrd-testbed/ec/ids/cut/DBX/NOVA/2/tls-server/server', {
+        ca:      'nrd-testbed/ec/ids/component/ca',
+        subject: {
+            country:      'GR',
+            organization: 'Nova',
+            commonName:   '*.nova.gr'
+        }
+    });
+};
+
+generators['nrd-testbed/ec/ids/cut/DBX/NOVA/3']
+    = async function (param) {
+    await generators['nrd-testbed/ec/ids/cut/DBX/NOVA/3/connector/client'](param);
+    await generators['nrd-testbed/ec/ids/cut/DBX/NOVA/3/tls-server/server'](param);
+};
+
+generators['nrd-testbed/ec/ids/cut/DBX/NOVA/3/connector/client']
+    = async function ({agent, config, defaults}) {
+    await agent.generateCustomerCertificate('nrd-testbed/ec/ids/cut/DBX/NOVA/3/connector/client', {
+        ca:      'nrd-testbed/ec/ids/component/ca',
+        subject: {
+            country:      'GR',
+            organization: 'Nova',
+            commonName:   '*.nova.gr'
+        }
+    });
+};
+
+generators['nrd-testbed/ec/ids/cut/DBX/NOVA/3/tls-server/server']
+    = async function ({agent, config, defaults}) {
+    await agent.generateCustomerCertificate('nrd-testbed/ec/ids/cut/DBX/NOVA/3/tls-server/server', {
+        ca:      'nrd-testbed/ec/ids/component/ca',
+        subject: {
+            country:      'GR',
+            organization: 'Nova',
+            commonName:   '*.nova.gr'
+        }
+    });
+};
+
+generators['nrd-testbed/ec/ids/cut/DBX/NOVA/4']
+    = async function (param) {
+    await generators['nrd-testbed/ec/ids/cut/DBX/NOVA/4/connector/client'](param);
+    await generators['nrd-testbed/ec/ids/cut/DBX/NOVA/4/tls-server/server'](param);
+};
+
+generators['nrd-testbed/ec/ids/cut/DBX/NOVA/4/connector/client']
+    = async function ({agent, config, defaults}) {
+    await agent.generateCustomerCertificate('nrd-testbed/ec/ids/cut/DBX/NOVA/4/connector/client', {
+        ca:      'nrd-testbed/ec/ids/component/ca',
+        subject: {
+            country:      'GR',
+            organization: 'Nova',
+            commonName:   '*.nova.gr'
+        }
+    });
+};
+
+generators['nrd-testbed/ec/ids/cut/DBX/NOVA/4/tls-server/server']
+    = async function ({agent, config, defaults}) {
+    await agent.generateCustomerCertificate('nrd-testbed/ec/ids/cut/DBX/NOVA/4/tls-server/server', {
+        ca:      'nrd-testbed/ec/ids/component/ca',
+        subject: {
+            country:      'GR',
+            organization: 'Nova',
+            commonName:   '*.nova.gr'
+        }
+    });
+};
+
+generators['nrd-testbed/ec/ids/cut/DBX/NOVA/5']
+    = async function (param) {
+    await generators['nrd-testbed/ec/ids/cut/DBX/NOVA/5/connector/client'](param);
+    await generators['nrd-testbed/ec/ids/cut/DBX/NOVA/5/tls-server/server'](param);
+};
+
+generators['nrd-testbed/ec/ids/cut/DBX/NOVA/5/connector/client']
+    = async function ({agent, config, defaults}) {
+    await agent.generateCustomerCertificate('nrd-testbed/ec/ids/cut/DBX/NOVA/5/connector/client', {
+        ca:      'nrd-testbed/ec/ids/component/ca',
+        subject: {
+            country:      'GR',
+            organization: 'Nova',
+            commonName:   '*.nova.gr'
+        }
+    });
+};
+
+generators['nrd-testbed/ec/ids/cut/DBX/NOVA/5/tls-server/server']
+    = async function ({agent, config, defaults}) {
+    await agent.generateCustomerCertificate('nrd-testbed/ec/ids/cut/DBX/NOVA/5/tls-server/server', {
+        ca:      'nrd-testbed/ec/ids/component/ca',
+        subject: {
+            country:      'GR',
+            organization: 'Nova',
+            commonName:   '*.nova.gr'
+        }
+    });
+};
+
+generators['nrd-testbed/ec/ids/cut/DBX/NOVA/6']
+    = async function (param) {
+    await generators['nrd-testbed/ec/ids/cut/DBX/NOVA/6/connector/client'](param);
+    await generators['nrd-testbed/ec/ids/cut/DBX/NOVA/6/tls-server/server'](param);
+};
+
+generators['nrd-testbed/ec/ids/cut/DBX/NOVA/6/connector/client']
+    = async function ({agent, config, defaults}) {
+    await agent.generateCustomerCertificate('nrd-testbed/ec/ids/cut/DBX/NOVA/6/connector/client', {
+        ca:      'nrd-testbed/ec/ids/component/ca',
+        subject: {
+            country:      'GR',
+            organization: 'Nova',
+            commonName:   '*.nova.gr'
+        }
+    });
+};
+
+generators['nrd-testbed/ec/ids/cut/DBX/NOVA/6/tls-server/server']
+    = async function ({agent, config, defaults}) {
+    await agent.generateCustomerCertificate('nrd-testbed/ec/ids/cut/DBX/NOVA/6/tls-server/server', {
+        ca:      'nrd-testbed/ec/ids/component/ca',
+        subject: {
+            country:      'GR',
+            organization: 'Nova',
+            commonName:   '*.nova.gr'
+        }
+    });
+};
+
+Object.freeze(generators);
+module.exports = generators;
